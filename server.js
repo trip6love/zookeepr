67847189b0686f418cc15eccaddf0f637a488754
trip6love <lanebyers13@gmail.com>
2021-12-15 15:44:55 -0500

    Front end style

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 const { animals } = require('./data/animals');
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -111,6 +113,20 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
